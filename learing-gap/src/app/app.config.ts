ad3e30be74e63347fe85cf790def4223f252435c
CristianGurauPXL <Cristian.Gurau@student.pxl.be>
2026-05-06 11:33:32 +0200

    Add config in app.config.ts

--- learing-gap/src/app/app.config.ts
+++ learing-gap/src/app/app.config.ts
@@ -6,6 +6,15 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
 import { environment } from '../env/environment';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCSLickASQdKMH4Y09_3P49L6jIlycxYN4",
+  authDomain: "learning-gap-547ae.firebaseapp.com",
+  projectId: "learning-gap-547ae",
+  storageBucket: "learning-gap-547ae.firebasestorage.app",
+  messagingSenderId: "1067257288108",
+  appId: "1:1067257288108:web:d5a262a33e7487611c1609"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
